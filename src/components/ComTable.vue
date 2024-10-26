b071ed8ef5b34fd53a496c5843ba84a5b72441fa
kb1ngram4 <1318947374@qq.com>
2024-10-26 20:07:52 +0800

    暴露子组件方法

--- src/components/ComTable.vue
+++ src/components/ComTable.vue
@@ -1,8 +1,9 @@
 <template>
-    <el-table :data="tableData" border style="width:100%" header-row-class-name="table-header-class">
+    <el-table ref="multipleTableRef" :data="tableData" border style="width:100%"
+        header-row-class-name="table-header-class" @selection-change="handleSelectionChange">
         <el-table-column :align="'center'" type="selection" width="50" v-if="selected" />
         <el-table-column :align="'center'" type="index" label="序号" width="55" v-if="indexd" />
-        <el-table-column v-for="item in tableTitle" :prop="item.prop" :label="item.label" 
+        <el-table-column v-for="item in tableTitle" :prop="item.prop" :label="item.label"
             :align="'center'"></el-table-column>
 
         <el-table-column fixed="right" align="center" label="操作" width="210">
@@ -18,23 +19,16 @@
             </div>
         </template>
     </el-table>
-    <div class="page">
-        <el-pagination
-        :current-page="pageNum"
-        :page-size="size"
-        background
-        :page-sizes="[5,10, 20, 50, 100]"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-    />
-    </div>
+    <!-- <div class="page">
+        <el-pagination :current-page="pageNum" :page-size="size" background :page-sizes="[5, 10, 20, 50, 100]"
+            layout="total, sizes, prev, pager, next, jumper" :total="total" @size-change="handleSizeChange"
+            @current-change="handleCurrentChange" />
+    </div> -->
 </template>
-<script lang="js" setup>
+<script setup>
 // const props = defineProps(['tableData','tableTitle','operation'])
-import {ref} from 'vue'
-const emit = defineEmits(['pagerFresh', 'submit'])
+import { ref, defineEmits, defineProps, defineExpose } from 'vue'
+const emit = defineEmits(['pagerFresh', 'submit', 'handleClearSelection', 'toggleInverseSelection', 'handleSelectionChange'])
 const props = defineProps({
     tableData: {
         type: Array,
@@ -52,60 +46,104 @@ const props = defineProps({
         type: Boolean,
         default: true
     },
-    indexd:{
-        type:Boolean,
-        default:true
+    indexd: {
+        type: Boolean,
+        default: true
     },
-    total:{
-        type:Number,
-        default:10
+    total: {
+        type: Number,
+        default: 10
     },
-    size:{
-        type:Number,
-        default:5,
+    size: {
+        type: Number,
+        default: 5,
     },
-    pageNum:{
-        type:Number,
-        default:1
+    pageNum: {
+        type: Number,
+        default: 1
     }
 
 })
 const tableData = props.tableData
-const tableTitle = props.tableTitle
-const pageNum = props.pageNum
-const size = props.size
-console.log(pageNum,size);
+// const tableTitle = props.tableTitle
+// const pageNum = props.pageNum
+// const size = props.size
+// console.log(pageNum, size);
 
 const handleC = () => { }
 
-let queryParams = ref({pageNum,size})
+
+
+const multipleSelection = ref([])
+const multipleTableRef = ref()
+const notIncludedArray = ref([])
+let submitArr = ref([])
+const handleSelectionChange = (val) => {
+    console.log(val);
+
+    console.log(val, tableData);
+
+    multipleSelection.value = val
+    submitArr.value = val
+    notIncludedArray.value = tableData.filter(item => !val.includes(item))
+    emit('handleSelectionChange', notIncludedArray)
+
+}
+//  反选
+const toggleInverseSelection = (rows) => {
+    console.log('rows', rows);
+
+    multipleTableRef.value.clearSelection()
+    if (rows) {
+        rows.forEach((row) => {
+            multipleTableRef.value.toggleRowSelection(row, undefined)
+
+        })
+        submitArr.value = rows
+
+    } else {
+        multipleTableRef.value.clearSelection()
+    }
+    emit('toggleInverseSelection', submitArr)
+}
+// 清除
+const handleClearSelection = () => {
+    multipleTableRef.value.clearSelection()
+    console.log(multipleSelection);
+
+}
+defineExpose({
+    handleSelectionChange, toggleInverseSelection, handleClearSelection
+})
+// let queryParams = ref({ pageNum, size })
 
 // 分页器处理函数
 // 单页数据条数改变
 function handleSizeChange(ev) {
-  queryParams.value.size = ev;
-  emit('pagerFresh', JSON.parse(JSON.stringify(queryParams.value)))
+    queryParams.value.size = ev;
+    emit('pagerFresh', JSON.parse(JSON.stringify(queryParams.value)))
 }
 // 页数改变
 function handleCurrentChange(ev) {
-  queryParams.value.pageNum = ev;
-  emit('pagerFresh', JSON.parse(JSON.stringify(queryParams.value)))
+    queryParams.value.pageNum = ev;
+    emit('pagerFresh', JSON.parse(JSON.stringify(queryParams.value)))
 }
 </script>
 <style lang="scss" scoped>
-
-.page{
+.page {
 
     display: flex;
     justify-content: center;
     margin: 20px 0px;
 }
+
 :deep(.table-header-class) th {
-  background-color: #f7fbfe;
-  color: #000;
+    background-color: #f7fbfe;
+    color: #000;
 }
+
 /* row */
 .el-table :deep(.el-table__cell) {
-  padding: 2px 0;
+    padding: 2px 0;
 }
 </style>
--- src/views/CarManage/CarRecord.vue
+++ src/views/CarManage/CarRecord.vue
@@ -1,6 +1,59 @@
 <template>
-<!-- 车辆档案 -->
+    <!-- 车辆档案 -->
     <div class="record">
         <h1>车辆档案</h1>
+        <el-button @click="reverseSelect">反选</el-button>
+        <el-button @click="clearSelect">清除</el-button>
+        <ComTable ref="tableRef" :tableTitle="tableTitle" :tableData="tableData"
+            @toggleInverseSelection="toggleInverseSelection" @handleClearSelection="handleClearSelection"
+            @handleSelectionChange="handleSelectionChange" />
     </div>
 </template>
+<script setup>
+import ComTable from '@/components/ComTable.vue';
+import { ref } from 'vue';
+const tableRef = ref()
+const allArr = ref([])
+const reverseSelect = (val) => {
+    console.log('sadsad', allArr.value);
+
+    tableRef.value.toggleInverseSelection(allArr.value)
+    // toggleInverseSelection(allArr.value)
+
+}
+const clearSelect = (val) => {
+    // console.log('sadsad', allArr.value);
+
+    tableRef.value.handleClearSelection(allArr.value)
+    // toggleInverseSelection(allArr.value)
+
+}
+const handleSelectionChange = (val) => {
+    console.log('勾选', val);
+    allArr.value = val.value
+
+}
+const toggleInverseSelection = (val) => {
+    console.log('反选21332131', val);
+    // allArr.value = val
+
+}
+const handleClearSelection = (val) => {
+    console.log('清除', val);
+    // allArr.value = val
+
+}
+
+const tableTitle = ref([
+    { prop: 'name', label: '姓名' },
+    { prop: 'age', label: '年龄' },
+    { prop: 'gender', label: '性别' },
+    { prop: 'address', label: '住址' },
+])
+const tableData = ref([
+    { name: 'zs1', age: 18, gender: '男', address: '上海' },
+    { name: 'zs2', age: 18, gender: '男', address: '上海' },
+    { name: 'zs3', age: 18, gender: '男', address: '上海' },
+    { name: 'zs4', age: 18, gender: '男', address: '上海' },
+])
+</script>
